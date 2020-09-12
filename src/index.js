e18efba13eab9b255f87f5eaa5f97431ff391ebe
joshtom <olajidejoshua4real@gmail.com>
2020-09-12 16:02:46 +0100

    I added global styling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React, { Fragment } from 'react';
-import ReactDom from 'react-dom';
-import PrimaryButton, { SecondaryButton, TertiaryButton } from './components/Buttons'
+import React, { Fragment } from "react";
+import ReactDom from "react-dom";
+import PrimaryButton, {
+  SecondaryButton,
+  TertiaryButton,
+} from "./components/Buttons";
+import { GlobalStyle } from './utils';
 
 function App() {
-    return(
-        <Fragment>
-        <PrimaryButton> Hello, Working </PrimaryButton>
-        <SecondaryButton> Secondary Button </SecondaryButton>
-        <TertiaryButton> Tertiary Button </TertiaryButton>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+        <GlobalStyle />
+      <PrimaryButton> Hello, Working </PrimaryButton>
+      <SecondaryButton> Secondary Button </SecondaryButton>
+      <TertiaryButton> Tertiary Button </TertiaryButton>
+    </Fragment>
+  );
 }
 
-ReactDom.render(<App />, document.querySelector("#root"))
+ReactDom.render(<App />, document.querySelector("#root"));
